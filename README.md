42d59f7e6a2da8f5a0af4ee334ab084a5af99bca
Aishwarya <aishwaryajais1996@gmail.com>
2025-05-14 20:29:26 +0530

    Implementng OAuth 2

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-Authentication - 4: Implementng OAuth 2
+### Authentication - 4: Implementng OAuth 2:
 
-OIDC stands for Open Id Connect which extends OAuth2. OIDC provides authentication and OAuth2 provides authorization. OIDC is a wrapper on top of OAuth2.
+OIDC stands for Open Id Connect which extends OAuth2. 
+OIDC provides authentication and OAuth2 provides authorization. 
+OIDC is a wrapper on top of OAuth2.
+
+JWK  stands for JSON Web Keys
+What is Single Sign On?
+
+Why do we inject Service in Controller and not create a new object?
+
+Jackson is the library that SpringBoot uses to convert an object to JSON and vice versa.
+Jackson has some problems:
+
+Refresh token is the token that we need to send to authorization server whenever the access token expires.
+Client requests server new access token with the help of refresh token whenever access token expires.
+The validity of access token is very less.
+
+nbf in jwt token stands for not valid before. We can change this value.
+
+Watch from 2:27 till end
+
+User data
+{
+"name": "Aishwarya",
+"email": "aish@test.com",
+"password": "pass1234"
+}
--- src/main/java/com/example/userservicecapstone/configs/SecurityConfiguration.java
+++ src/main/java/com/example/userservicecapstone/configs/SecurityConfiguration.java
@@ -2,27 +2,27 @@ package com.example.userservicecapstone.configs;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Component;
 
-@Component
-public class SecurityConfiguration {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> {
-                            try {
-                                requests
-                                        .anyRequest().permitAll()
-                                        .and().cors().disable()
-                                        .csrf().disable();
-                            } catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                );
-
-        return http.build();
-    }
-}
+//@Configuration
+//public class SecurityConfiguration {
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((requests) -> {
+//                            try {
+//                                requests
+//                                        .anyRequest().permitAll()
+//                                        .and().cors().disable()
+//                                        .csrf().disable();
+//                            } catch (Exception e) {
+//                                throw new RuntimeException(e);
+//                            }
+//                        }
+//                );
+//
+//        return http.build();
+//    }
+//}
